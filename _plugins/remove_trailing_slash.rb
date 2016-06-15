1c5bbccaaab8772b00e1f59a96c01487a35211ad
Yucheng Chuang <yorkxin@Shinagawa-II.local>
2016-06-15 12:04:27 +0800

    remove Facebook and Twitter widget

--- _plugins/remove_trailing_slash.rb
+++
@@ -1,9 +0,0 @@
-module Jekyll
-  module RemoveTrailingSlash
-    def remove_trailing_slash(input)
-      input.sub(/\/$/,'')
-    end
-  end
-end
-
-Liquid::Template.register_filter(Jekyll::RemoveTrailingSlash)
